f5b6eea16970c7c82cb2ed413245f6748ee852eb
satya kavya <satyakavya24@gmail.com>
2020-10-21 21:35:19 -0500

    create get and post methods

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,54 @@
+var path = require("path");
+var fs = require("fs");
+// var toJson = { allNotes:[]}
+
+module.exports = function(app) {
+    var notes = {
+        allNotes: []
+     };
+   
+    app.get("/api/notes", function(req, res) {
+      fs.readFile("db.json","utf8",function readFile(err,data){
+          if(err){
+              console.log(err);
+          }
+          else{
+              console.log(data)
+              return res.json(JSON.parse(data));
+          }
+      })
+    });
+  
+  
+  
+    // If no matching route is found default to home
+    
+
+
+  app.post("/api/notes", function(req, res) {
+    //   toJson.push(JSON.stringify(req.body))
+    notes.allNotes.push(req.body);
+    var newJson = JSON.stringify(notes)
+    
+    
+    // var saveJson = JSON.stringify(toJson, null, 4)
+
+        fs.writeFile('db.json', newJson, 'utf8', (err)=>{
+            if(err){
+                console.log(err)
+            }
+        })
+
+    //  var newData = JSON.stringify(req.body);
+    //   db.json.push(newData);
+     res.json(true);
+    
+    
+ 
+    });
+
+    app.delete("/api/notes/:id",function(req,res){
+        var id = req.params.id;
+
+    })
+};
